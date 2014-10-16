ca5f58cff07a4f7eeb2e2d7a3cdc5c6ac20fdd4a
Pedro Teixeira <i@pgte.me>
2014-10-16 16:03:39 +0100

    abstract connection

--- test.js
+++ test.js
@@ -31,3 +31,50 @@ test('transport listen is extendable', function(t) {
 
   function callback() {}
 });
+
+test('connection send is extensible', function(t) {
+  var c = new AST.Connection();
+
+  var type = 'type';
+  var args = {a:1, b:2};
+  function callback() {}
+
+  c._send = function(_type, _args, f) {
+    t.equal(_type, type);
+    t.equal(_args, args);
+    t.equal(f, callback);
+    t.end();
+  };
+
+  c.send(type, args, callback);
+});
+
+test('connection receive is extensible', function(t) {
+  var c = new AST.Connection();
+
+  var type = 'type';
+  var args = {a:1, b:2};
+  function callback() {}
+
+  c._send = function(_type, _args, f) {
+    t.equal(_type, type);
+    t.equal(_args, args);
+    t.equal(f, callback);
+    t.end();
+  };
+
+  c.send(type, args, callback);
+});
+
+test('connection close is extensible', function(t) {
+  var c = new AST.Connection();
+
+  function callback() {}
+
+  c._close = function(_callback) {
+    t.equal(_callback, callback);
+    t.end();
+  };
+
+  c.close(callback);
+});
