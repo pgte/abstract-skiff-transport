8ec82dc349a695c25a6e28f388e57a1003dccad4
Pedro Teixeira <i@pgte.me>
2014-10-21 17:10:40 +0100

    fixed response interface

--- test/send.js
+++ test/send.js
@@ -8,11 +8,12 @@ function sendTest(transport, options) {
   test('can send', function(t) {
     var type = 'type';
     var args = {a: 1, b: 2};
-    options._connection.send(type, args, function(err, resp) {
+    options._connection.send(type, args, function(err, _type, _args) {
       if (err) {
         throw err;
       }
-      t.deepEqual(resp, [type, args]);
+      t.equal(_type, type);
+      t.deepEqual(_args, args);
       t.end();
     });
   });
